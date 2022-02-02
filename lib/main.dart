70cc3a68aed7945b8f85b598c27a50e3ac136529
kotahorii <khorii.micron@gmail.com>
2022-02-02 13:35:35 +0900

    [Update] login page

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: '認証画面'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final String _email = '';
-  final String _password = '';
+  var email = '';
+  var password = '';
+  var isLogin = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(isLogin ? 'ログイン' : '新規登録'),
       ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -47,19 +49,34 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: const InputDecoration(hintText: 'Enter your email'),
             ),
             const SizedBox(
-              height: 10,
+              height: 20,
             ),
             TextFormField(
               decoration:
                   const InputDecoration(hintText: 'Enter your password'),
+              onChanged: (value) => setState(() => password = value),
             ),
             const SizedBox(
-              height: 10,
+              height: 20,
             ),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => const TaskList())),
-              child: const Text('submit'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                          builder: (context) => const TaskList())),
+                  child: const Text('submit'),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                TextButton(
+                    onPressed: () => setState(() {
+                          isLogin = !isLogin;
+                        }),
+                    child: Text(isLogin ? '新規登録はこちら' : 'ログインはこちら')),
+              ],
             )
           ],
         ),
